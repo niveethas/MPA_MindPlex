695e0a2320e3f3ab1843d9d2dc2eda19d78b7c78
niveethas <61502706+niveethas@users.noreply.github.com>
2023-01-27 12:39:26 +0000

    Sign Up functionality working

--- app/src/main/java/com/example/coursework_mindplex/SignUpActivity.java
+++ app/src/main/java/com/example/coursework_mindplex/SignUpActivity.java
@@ -11,24 +11,32 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
+    //auth.getcurrentid
 
     //****** Use authentication to log user in and then use the unique id to get the user information from db ******
-    public void signup(String email, String password){
+    public void signup(String email, String password, String firstName, String lastName){
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult>
@@ -37,7 +45,22 @@ public class SignUpActivity extends AppCompatActivity {
                                     Log.d("MainActivity",
                                             "createUserWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
+                                    String uid = user.getUid();
                                     //****TO-DO: add activity ******
+                                    Map<String,Object> userDetails = new HashMap<>();
+                                    userDetails.put("First Name", firstName);
+                                    userDetails.put("Last Name",lastName);
+                                    userDetails.put("Anxiety Advice", "true");
+                                    userDetails.put("Camera", "true");
+                                    userDetails.put("Dark Mode", "false");
+                                    userDetails.put("Location", "true");
+                                    db.collection("Users").document(uid).set(userDetails)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+                                                    Log.d("SignUpActivity","User written");
+                                                }
+                                            })   ;
                                     Intent SUMenu = new Intent(SignUpActivity.this, MainMenu.class);
                                     startActivity(SUMenu);
                                 } else {
@@ -55,9 +78,14 @@ public class SignUpActivity extends AppCompatActivity {
     public void registrationButtonClicked(View view) {
         EditText email = findViewById(R.id.emailTxtbox);
         EditText password = findViewById(R.id.passwordTxtbox);
+        EditText fname = findViewById(R.id.FNameTxtbox);
+        EditText lname = findViewById(R.id.LNameTxtbox);
         String sEmail = email.getText().toString();
         String sPassword = password.getText().toString();
-        signup(sEmail, sPassword);
+        String sFName = fname.getText().toString();
+        String sLName = lname.getText().toString();
+        Log.d("Sign in: ", sEmail + sPassword + sFName + sLName);
+        signup(sEmail, sPassword, sFName, sLName);
     }
 
 
