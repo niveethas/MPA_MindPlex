383eeeebdf614db1943305c5f4fe5ad809dfb3eb
niveethas <61502706+niveethas@users.noreply.github.com>
2023-01-27 12:59:15 +0000

    Added sign up validation

--- app/src/main/java/com/example/coursework_mindplex/SignUpActivity.java
+++ app/src/main/java/com/example/coursework_mindplex/SignUpActivity.java
@@ -33,38 +33,48 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
-    //auth.getcurrentid
-
-    //****** Use authentication to log user in and then use the unique id to get the user information from db ******
+// The following code has been derived from LAB 10 and LAB 11
     public void signup(String email, String password, String firstName, String lastName){
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult>
                                                            task) {
                                 if (task.isSuccessful()) {
-                                    Log.d("MainActivity",
-                                            "createUserWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    String uid = user.getUid();
-                                    //****TO-DO: add activity ******
-                                    Map<String,Object> userDetails = new HashMap<>();
-                                    userDetails.put("First Name", firstName);
-                                    userDetails.put("Last Name",lastName);
-                                    userDetails.put("Anxiety Advice", "true");
-                                    userDetails.put("Camera", "true");
-                                    userDetails.put("Dark Mode", "false");
-                                    userDetails.put("Location", "true");
-                                    db.collection("Users").document(uid).set(userDetails)
-                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                @Override
-                                                public void onSuccess(Void unused) {
-                                                    Log.d("SignUpActivity","User written");
-                                                }
-                                            })   ;
-                                    Intent SUMenu = new Intent(SignUpActivity.this, MainMenu.class);
-                                    startActivity(SUMenu);
+                                    try {
+                                        String uid = user.getUid();
+                                        //****TO-DO: add activity ******
+                                        Map<String, Object> userDetails = new HashMap<>();
+                                        userDetails.put("First Name", firstName);
+                                        userDetails.put("Last Name", lastName);
+                                        userDetails.put("Anxiety Advice", "true");
+                                        userDetails.put("Camera", "true");
+                                        userDetails.put("Dark Mode", "false");
+                                        userDetails.put("Location", "true");
+                                        db.collection("Users").document(uid).set(userDetails)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void unused) {
+                                                        Log.d("SignUpActivity", "User written");
+                                                    }
+                                                });
+                                        Intent SUMenu = new Intent(SignUpActivity.this, MainMenu.class);
+                                        startActivity(SUMenu);
+                                    }
+                                    catch (Exception e) {
+                                        //REMOVE THE USER FROM AUTHENTICATION IF IT COULD NOT BE SAVED IN FIRESTORE
+                                        try {
+                                            user.delete();
+                                            Toast.makeText(SignUpActivity.this,
+                                                    "Sign Up failed.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }catch (Exception ex){
+                                            Log.w("SignUpActivity",
+                                                    "deleteUser:failure", task.getException());
+                                        }
+                                    }
                                 } else {
-                                    // If sign in fails, display a message to the user.
+                                    // Log failure and send user a pop up to notify failure
                                     Log.w("MainActivity",
                                             "createUserWithEmail:failure", task.getException());
                                     Toast.makeText(SignUpActivity.this,
@@ -78,14 +88,21 @@ public class SignUpActivity extends AppCompatActivity {
     public void registrationButtonClicked(View view) {
         EditText email = findViewById(R.id.emailTxtbox);
         EditText password = findViewById(R.id.passwordTxtbox);
+        EditText rpassword = findViewById(R.id.RpasswordTxtbox);
         EditText fname = findViewById(R.id.FNameTxtbox);
         EditText lname = findViewById(R.id.LNameTxtbox);
         String sEmail = email.getText().toString();
         String sPassword = password.getText().toString();
         String sFName = fname.getText().toString();
         String sLName = lname.getText().toString();
-        Log.d("Sign in: ", sEmail + sPassword + sFName + sLName);
-        signup(sEmail, sPassword, sFName, sLName);
+        if (rpassword.getText().toString() == sPassword) {
+            signup(sEmail, sPassword, sFName, sLName);
+        }
+        else{
+            Toast.makeText(SignUpActivity.this,
+                "Passwords Do Not Match.",
+                Toast.LENGTH_SHORT).show();
+        }
     }
 
 
