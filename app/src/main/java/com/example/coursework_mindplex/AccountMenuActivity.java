e97fd2443741c919d3d576146a8cbb653200908e
niveethas <61502706+niveethas@users.noreply.github.com>
2023-02-22 22:10:39 +0000

    Sign Out function

--- app/src/main/java/com/example/coursework_mindplex/AccountMenuActivity.java
+++ app/src/main/java/com/example/coursework_mindplex/AccountMenuActivity.java
@@ -6,12 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AccountMenuActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_menu);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void passwordChangeActivity(View view){
@@ -22,5 +27,11 @@ public class AccountMenuActivity extends AppCompatActivity {
     public void personalisationActivity(View view){
         Intent AMPersonalisation = new Intent(AccountMenuActivity.this, PersonalisationActivity.class);
         startActivity(AMPersonalisation);
+    }
+
+    public void signOutActivity(View view){
+        mAuth.signOut();
+        Intent AMSignOut = new Intent(AccountMenuActivity.this, MainActivity.class);
+        startActivity(AMSignOut);
     }
 }
