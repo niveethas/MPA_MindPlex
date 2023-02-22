7ddf888799ddd3b72d95ddb75137309ae220bb21
niveethas <61502706+niveethas@users.noreply.github.com>
2023-02-22 22:09:47 +0000

    Personal Info Screen added

---
+++ app/src/main/java/com/example/coursework_mindplex/ChangePersonalInfo.java
@@ -0,0 +1,72 @@
+package com.example.coursework_mindplex;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ChangePersonalInfo extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    public String uid;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    public String uId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_personal_info);
+        uId = user.getUid();
+    }
+
+
+    public void changePersonalInfo(View view){
+        EditText newFname = findViewById(R.id.editFNameTxt);
+        EditText newLname = findViewById(R.id.editLNameTxt);
+
+        if (!toString().valueOf(newFname.getText()).isEmpty()) {
+            db.collection("Users").document(uId)
+                    .update("First Name", toString().valueOf(newFname.getText()))
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(ChangePersonalInfo.this, "Name Updated", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("Personalisation", "Error updating document", e);
+                        }
+                    });
+        }
+        if (!toString().valueOf(newLname.getText()).isEmpty()) {
+            db.collection("Users").document(uId)
+                    .update("Last Name", toString().valueOf(newFname.getText()))
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(ChangePersonalInfo.this, "Name Updated", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("Personalisation", "Error updating document", e);
+                        }
+                    });
+        }
+    }
+
+}
