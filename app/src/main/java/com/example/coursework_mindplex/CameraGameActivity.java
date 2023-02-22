9c863243d9fd3f5b052f24054a00ee592903508b
niveethas <61502706+niveethas@users.noreply.github.com>
2023-02-22 14:32:17 +0000

    restrict camera game on permissions

--- app/src/main/java/com/example/coursework_mindplex/CameraGameActivity.java
+++ app/src/main/java/com/example/coursework_mindplex/CameraGameActivity.java
@@ -1,5 +1,6 @@
 package com.example.coursework_mindplex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -11,6 +12,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,21 +32,42 @@ public class CameraGameActivity extends AppCompatActivity {
     private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
     private static final int CAMERA_REQUEST_CODE = 10;
     public List<String> colorPicker = Arrays.asList("Blue","White","Red","Green","Black","Pink","Orange","Yellow");
+    private FirebaseAuth mAuth;
+    public String uid;
+    public String status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_game);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         Button enableCamera = findViewById(R.id.enableCamera);
+        FirebaseUser user = mAuth.getCurrentUser();
+        uid = user.getUid();
         setColourText();
         enableCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (hasCameraPermission()) {
-                    enableCamera();
-                } else {
-                    requestPermission();
-                }
+                db.collection("Users").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                status = document.getString("Camera");
+                            }
+                            if (status.equals("true")) {
+                                if (hasCameraPermission()) {
+                                    enableCamera();
+                                } else {
+                                    requestPermission();
+                                }
+                            }else{
+                                Toast.makeText(CameraGameActivity.this, "Please change camera permissions to play!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
             }
         });
     }
